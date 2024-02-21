11a8df2dd3b9ca698cb768ac6884aead5eddb02e
Alex Chandra Hanif <alexchandra1998@gmail.com>
2024-02-21 09:36:41 +0700

    feat: first commit

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+        crois: ["Croissant One", "cursive"],
+        play: ["Playfair Display", "serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        primary: "#0b282c",
+        secondary: "#00f6ff",
+        gold: "#C58940",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+        navbar: "#4D4D4D",
+        darkBrown: "#5E3C24",
+        brown: "#5E3C24",
+        background1: "#F8F7F2",
+        text: "#5D5D5D",
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1024px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [
+    {
+      autoprefixer: {},
+    },
+  ],
+};
